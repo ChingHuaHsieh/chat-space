87de770a141d387749c5cfa84d0fcd5da6d90fa7
ChingHuaHsieh <link0319@yahoo.com.tw>
2019-11-21 15:49:19 +0900

    add_user_function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:new, :create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def authenticate_user!
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
